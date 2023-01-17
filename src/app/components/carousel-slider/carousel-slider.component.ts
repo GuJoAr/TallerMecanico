886c99cf455c7c43b923386985e0dad51a511e0f
GuJoAr <110123911+GuJoAr@users.noreply.github.com>
2023-01-17 14:13:31 -0300

    cambio v15

--- src/app/components/carousel-slider/carousel-slider.component.ts
+++ src/app/components/carousel-slider/carousel-slider.component.ts
@@ -15,34 +15,22 @@ export class CarouselSliderComponent implements OnInit {
   imgCollection: Array<object> = [
     {
       image: '/assets/carousel/auto taller 01.jpg',
-      thumbImage: '/assets/carousel/auto taller 01.jpg',
-      alt: 'Image 1',
-      title: 'Image 1'
+      thumbImage: '/assets/carousel/auto taller 01.jpg',      
     }, {
       image: '/assets/carousel/auto taller 02.webp',
-      thumbImage: '/assets/carousel/auto taller 02.webp',
-      title: 'Image 2',
-      alt: 'Image 2'
+      thumbImage: '/assets/carousel/auto taller 02.webp',      
     }, {
       image: '/assets/carousel/auto taller 03.webp',
-      thumbImage: '/assets/carousel/auto taller 03.webp',
-      title: 'Image 3',
-      alt: 'Image 3'
+      thumbImage: '/assets/carousel/auto taller 03.webp',      
     }, {
       image: '/assets/carousel/auto taller 04.webp',
-      thumbImage: '/assets/carousel/auto taller 04.webp',
-      title: 'Image 4',
-      alt: 'Image 4'
+      thumbImage: '/assets/carousel/auto taller 04.webp',      
     }, {
       image: '/assets/carousel/auto taller 05.jpg',
-      thumbImage: '/assets/carousel/auto taller 05.jpg',
-      title: 'Image 5',
-      alt: 'Image 5'
+      thumbImage: '/assets/carousel/auto taller 05.jpg',      
     }, {
       image: '/assets/carousel/auto taller 06.jpg',
       thumbImage: '/assets/carousel/auto taller 06.jpg',
-      title: 'Image 6',
-      alt: 'Image 6'
     } 
   ];
 
